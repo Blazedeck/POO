7f767239987fb1d0a54944952897aff731678020
Blazedeck <90530422+Blazedeck@users.noreply.github.com>
2021-09-15 17:16:30 -0300

    Tabuada - Semana 1
    Exercídio de tabuada em Java requisitado na primeira aula de Programação Orientada a Objetos

---
+++ Tabuada.java
@@ -0,0 +1,14 @@
+/*Programação Orientada a Objetos - Semana 1
+Nome: Rodrigo Albani Orphão
+Matrícula: 0050016761
+*/
+public class Tabuada {
+
+    public static void main(String[] args) {
+
+      for(int i=1; i<11; i++){
+        System.out.println(10 * i);
+      }
+    }
+    
+}
